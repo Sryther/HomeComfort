522c2dbb5b90aebdb189894f408df464aeb2ece2
Paul Rey <paul@iterop.com>
2022-01-19 21:28:10 +0100

    Update mongodb info

--- src/server/data/database-instance.ts
+++ src/server/data/database-instance.ts
@@ -1,8 +1,13 @@
 import mongoose, {ConnectOptions} from "mongoose";
 import Config from "../config";
 
+const options: ConnectOptions = {
+    authMechanism: "SCRAM-SHA-256",
+};
+
 mongoose.connect(
-    `mongodb://${Config.database.credentials.username}:${Config.database.credentials.password}@${Config.database.host}:${Config.database.port}/${Config.database.dbname}`
+    `mongodb://${Config.database.credentials.username}:${Config.database.credentials.password}@${Config.database.host}:${Config.database.port}/${Config.database.dbname}?authSource=${Config.database.credentials.authDatabase}`,
+    options
 );
 
 export default mongoose;
